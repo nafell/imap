7eae22ef46e41f556b42d1d898801691b95e5a85
nafell <pixel68.43@gmail.com>
2023-09-27 16:00:10 +0900

    seed file init

---
+++ prisma/seed.ts
@@ -0,0 +1,17 @@
+import prisma from "@/lib/prisma";
+
+const options = {
+  environment: { type: "string" },
+};
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
