c9a44496532f0e27b33d4e63b0157e646d012308
nafell <pixel68.43@gmail.com>
2023-10-16 03:02:16 +0900

    Page: generateMetadata is now at the correct place layout->page

--- src/app/dish/[id]/page.tsx
+++ src/app/dish/[id]/page.tsx
@@ -23,6 +23,19 @@ export async function generateStaticParams() {
   }));
 }
 
+/** @see{@link https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config} */
+export const dynamicParams = false; // return a 404 page if the params are not found
+
+/** @see{@link https://nextjs.org/docs/app/api-reference/functions/generate-metadata} */
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const dish = (await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v-beta/dish/${params.id}`
+  ).then((res) => res.json())) as Dish;
+  return {
+    title: `${dish.name} & ${dish.restaurant.name}`,
+  };
+}
+
 async function getDish(id: string) {
   const dish = (await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v-beta/dish/${id}`)
     .then((res) => res.json())
